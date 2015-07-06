c9ff143ac12645c0c1c4314ebd25359644562f1a
massimo <massimo@debian>
2015-07-06 21:38:57 +0200

    update

--- nbattle_server.c
+++ nbattle_server.c
@@ -5,6 +5,12 @@
 #include <errno.h>
 #include <string.h>
 
+
+void gestisci_client (int cfd) {
+		
+	printf("Nuova Connessione\n");		
+}
+
 int main(){
 	
 	//struct per il bind 
@@ -18,6 +24,7 @@ int main(){
 		return -1;//xke devo uscire dal programma
 	}
 	server_addr.sin_addr.s_addr= INADDR_ANY;
+	server_addr.sin_family=AF_INET;
 	server_addr.sin_port= htons(5555);
 	err=bind(lfd,(struct sockaddr*)&server_addr, sizeof(server_addr));
 	
@@ -25,8 +32,28 @@ int main(){
 		perror("bind()");
 		return -1;//xke devo uscire dal programma
 	}
+	//una volta che err è 0, la posso riusare
+	err=listen(lfd,10);
+	if(err) {
+		perror("listen()");
+		return -1;//xke devo uscire dal programma
+	}
+	//creo un for per accettare molte connessioni che mi vengono richeste
+	for(;;){
+		int cfd;//client file descriptor
+		socklen_t addrlen;
+		struct sockaddr_in client_addr;
+//ritorna un file descriptor, associato alla connessione delclient
+		cfd=accept(lfd,(struct sockaddr*)&client_addr, &addrlen);
+		if(cfd<0) {
+			perror("accept()");
+			break;
+		}
+		gestisci_client(cfd);
+		close(cfd);
+	}
 	
 	
 
-return 0;
+	return 0;
 }
