ad8a6858c2f5993aecbf6012f9204787b74f372a
massimo <massimo@debian>
2015-07-06 22:26:28 +0200

    update

--- Makefile
+++ Makefile
@@ -1,16 +1,16 @@
 all: nbattle_client nbattle_server
 
 nbattle_client: nbattle_client.o
-	gcc -o nbattle_client nbattle_client.o
+	gcc -Wall -o nbattle_client nbattle_client.o
 
 nbattle_server: nbattle_server.o
-	gcc -o nbattle_server nbattle_server.o
+	gcc -Wall -o nbattle_server nbattle_server.o -lpthread
 
 nbattle_client.o: nbattle_client.c
-	gcc -c nbattle_client.c
+	gcc -Wall -c nbattle_client.c
 
 nbattle_server.o: nbattle_server.c
-	gcc -c nbattle_server.c
+	gcc -Wall -c nbattle_server.c
 
 clean:
 	-rm *.o nbattle_client nbattle_server
--- nbattle_server.c
+++ nbattle_server.c
@@ -4,19 +4,50 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <pthread.h>
 
+#define NUM_THREADS 5
+//struttura informazioni relative ad un thread nel pool
+struct gestore {
+	pthread_t th;	
+	pthread_cond_t newrequest;
+	pthread_mutex_t lock;
+};
 
-void gestisci_client (int cfd) {
-		
-	printf("Nuova Connessione\n");		
+void * gestisci_client(void *arg){
+	
+	struct gestore* g=arg;
+	for(;;){
+//prendo il lock per utilizzare la variabile codition
+		pthread_mutex_lock(&g->lock);
+//aspetta che arrivi l segnale nel frattempo si blocca rilasciano il lock
+		pthread_cond_wait(&g->newrequest,&g->lock);
+		pthread_mutex_unlock(&g->lock);
+		printf("Ho gestito la connessione\n");	
+	};	
+	return NULL;
 }
 
 int main(){
-	
+	//array di varibaili pthread_t di tipo opaco(non sappiamo come è fatto dentro
+	struct gestore pool[NUM_THREADS];	
 	//struct per il bind 
 	struct sockaddr_in server_addr;	
 	int lfd; // listening file descriptor per il socket 
-	int err;	
+	int err;
+	int i;
+	int curr_thread=0; //thread corrente	
+	
+	for(i=0; i< NUM_THREADS; i++) {
+		err=pthread_create(&pool[i].th, NULL, gestisci_client, &pool[i]);
+		if (err){
+			perror("phtread_create()");
+			return -1;//xke devo uscire dal programma
+		}
+		pthread_cond_init(&pool[i].newrequest, NULL);
+		pthread_mutex_init(&pool[i].lock, NULL);
+	}
+	
 	//lo scopo del sk è di ascoltare connessioni dei client
 	lfd=socket (AF_INET, SOCK_STREAM,0);
 	if (lfd<0){
@@ -49,8 +80,11 @@ int main(){
 			perror("accept()");
 			break;
 		}
-		gestisci_client(cfd);
-		close(cfd);
+//sveglio il thread i esimo che si era bloccato nella wait
+		pthread_mutex_lock(&pool[curr_thread].lock);
+		pthread_cond_signal (&pool[curr_thread].newrequest);		
+		pthread_mutex_unlock(&pool[curr_thread].lock);
+		curr_thread=(curr_thread+1)%NUM_THREADS;
 	}
 	
 	
