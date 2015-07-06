0f2156ffe09deda305f277927344c84e197765e7
massimo <massimo@debian>
2015-07-06 21:01:31 +0200

    imore

--- nbattle_server.c
+++ nbattle_server.c
@@ -1,4 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
 int main(){
+	
+	//struct per il bind 
+	struct sockaddr_in server_addr;	
+	int lfd; // listening file descriptor per il socket 
+	int err;	
+	//lo scopo del sk è di ascoltare connessioni dei client
+	lfd=socket (AF_INET, SOCK_STREAM,0);
+	if (lfd<0){
+		perror("socket()");
+		return -1;//xke devo uscire dal programma
+	}
+	server_addr.sin_addr.s_addr= INADDR_ANY;
+	server_addr.sin_port= htons(5555);
+	err=bind(lfd,(struct sockaddr*)&server_addr, sizeof(server_addr));
+	
+	if(err) {
+		perror("bind()");
+		return -1;//xke devo uscire dal programma
+	}
+	
+	
 
 return 0;
 }
